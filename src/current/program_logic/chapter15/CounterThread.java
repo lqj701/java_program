51bf1314f9acf99db06a8a936af97d9319397690
李强进 <li.qj@ikcrm.com>
2019-03-22 10:54:41 +0800

    上传

---
+++ src/current/program_logic/chapter15/CounterThread.java
@@ -0,0 +1,25 @@
+package program_logic.chapter15;
+
+public  class CounterThread extends Thread {
+    private static volatile int counter= 0;
+
+    @Override
+    public  void run(){
+            for (int i = 0; i < 1000; i++) {
+                counter++;
+            }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        int num =1000;
+        Thread[]threads=new Thread[num];
+        for(int i=0;i<num;i++){
+            threads[i] = new CounterThread();
+            threads[i].start();
+        }
+        for(int i = 0;i<num;i++){
+            threads[i].join();
+        }
+        System.out.println(counter);
+    }
+}
---
+++ src/current/program_logic/chapter15/VisibilityDemo.java
@@ -0,0 +1,24 @@
+package program_logic.chapter15;
+
+public class VisibilityDemo {
+    private static boolean shutdown = false;
+
+    static class HelloThread extends Thread {
+        @Override
+        public void run() {
+            while (!shutdown) {
+
+            }
+            System.out.println("exit hello");
+        }
+
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        new HelloThread().start();
+        Thread.sleep(1000);
+        shutdown = true;
+        System.out.println("exit main");
+    }
+
+}
